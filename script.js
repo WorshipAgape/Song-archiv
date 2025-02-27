733f15b06c5c7c081819b6db83e1dffa33057786
Agape <153385364+exserega@users.noreply.github.com>
2025-02-27 17:05:54 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBlkjVQFtFpMRFexAi6nBqEkIfjFlU5cDo",
+  authDomain: "song-archive-389a6.firebaseapp.com",
+  projectId: "song-archive-389a6",
+  storageBucket: "song-archive-389a6.firebasestorage.app",
+  messagingSenderId: "619735277668",
+  appId: "1:619735277668:web:51d2684bd8d4444eaf3f71",
+  measurementId: "G-Z6QYH5YD2E"
+};
 
 
 // Initialize Firebase
